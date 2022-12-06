8e536024d5ea1fb818558763cf5cdb45e001e8e9
phuonglai99 <plai34037@gmail.com>
2022-12-06 23:13:10 +0700

    update class CalculateBMI to fix bug

--- src/lab_lesson_2/CalculateBMI.java
+++ src/lab_lesson_2/CalculateBMI.java
@@ -14,21 +14,21 @@ public class CalculateBMI {
         if (height == 0) {
             System.out.println("Wrong input height, it must be other than 0!");
         } else {
-            float BMI = weight / height / height;
+            float BMI = weight / height / 2;
             System.out.printf("Your BMI is: %f\n", BMI);
             if (BMI < 18.5) {
                 //calculate amount of weight need to increase which compared to current weight
-                float increaseWeight = (18.5f * height * height) - weight;
+                float increaseWeight = (18.5f * height * 2) - weight;
                 System.out.printf("Underweight \nTry to increase your weight by %f kg to reach normal weight", increaseWeight);
             } else if (BMI >= 18.5 && BMI <= 24.9) {
                 System.out.println("Normal weight \nKeep your weight");
             } else if (BMI >= 25 && BMI <= 29.9) {
                 //calculate amount of weight need to decrease which compared to current weight
-                float decreaseWeight = weight - (24.9f * height * height);
+                float decreaseWeight = weight - (24.9f * height * 2);
                 System.out.printf("Overweight \nTry to decrease your weight by %f kg to reach normal weight", decreaseWeight);
             } else {
                 //calculate amount of weight need to decrease which compared to current weight
-                float decreaseWeight = weight - (24.9f * height * height);
+                float decreaseWeight = weight - (24.9f * height * 2);
                 System.out.printf("Obesity \nTry to decrease your weight by %f kg to reach normal weight", decreaseWeight);
             }
         }
