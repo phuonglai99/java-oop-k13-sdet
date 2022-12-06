4f47c4223b6645c5653e5e30cac9c43ff67059f6
phuonglai99 <plai34037@gmail.com>
2022-12-06 23:06:47 +0700

    update class CalculateBMI to fix bug

--- src/lab_lesson_2/CalculateBMI.java
+++ src/lab_lesson_2/CalculateBMI.java
@@ -1,5 +1,6 @@
 package lab_lesson_2;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class CalculateBMI {
@@ -13,22 +14,24 @@ public class CalculateBMI {
         if (height == 0) {
             System.out.println("Wrong input height, it must be other than 0!");
         } else {
+            //DecimalFormat df = new DecimalFormat("###.#");
             float BMI = weight / height / height;
-            System.out.printf("Your BMI is: %.1f\n", BMI);
+            //BMI = df.format(BMI);
+            System.out.printf("Your BMI is: %f\n", BMI);
             if (BMI < 18.5) {
                 //calculate amount of weight need to increase which compared to current weight
                 float increaseWeight = (18.5f * height * height) - weight;
-                System.out.printf("Underweight \nTry to increase your weight by %.1f kg to reach normal weight", increaseWeight);
+                System.out.printf("Underweight \nTry to increase your weight by %f kg to reach normal weight", increaseWeight);
             } else if (BMI >= 18.5 && BMI <= 24.9) {
                 System.out.println("Normal weight \nKeep your weight");
             } else if (BMI >= 25 && BMI <= 29.9) {
                 //calculate amount of weight need to decrease which compared to current weight
                 float decreaseWeight = weight - (24.9f * height * height);
-                System.out.printf("Overweight \nTry to decrease your weight by %.1f kg to reach normal weight", decreaseWeight);
+                System.out.printf("Overweight \nTry to decrease your weight by %f kg to reach normal weight", decreaseWeight);
             } else {
                 //calculate amount of weight need to decrease which compared to current weight
                 float decreaseWeight = weight - (24.9f * height * height);
-                System.out.printf("Obesity \nTry to decrease your weight by %.1f kg to reach normal weight", decreaseWeight);
+                System.out.printf("Obesity \nTry to decrease your weight by %f kg to reach normal weight", decreaseWeight);
             }
         }
     }
