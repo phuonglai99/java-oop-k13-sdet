4c5a02146f6a487905eb6dfe0d6e524110739370
phuonglai99 <plai34037@gmail.com>
2022-12-16 22:51:19 +0700

    optimize code for lab_3_4

--- src/lab_lesson_3/lab_3_4.java
+++ src/lab_lesson_3/lab_3_4.java
@@ -3,41 +3,30 @@ package lab_lesson_3;
 public class lab_3_4 {
     //merge two ordered arrays//
     public static void main(String[] args) {
-        int[] arr1 = {1, 2, 3, 4, 5};
-        //boolean[] flag1 = new boolean[arr1.length];
-        int[] arr2 = {4,5,6,8,9};
-        //boolean[] flag2 = new boolean[arr2.length];
-        int[] mergeArray = new int[arr1.length + arr2.length];
-        int indexArr1 = 0, indexArr2 = 0, indexMergeArray = 0;
-
-        while (indexArr1 < arr1.length && indexArr2 < arr2.length) {
-            if (arr1[indexArr1] <= arr2[indexArr2]) {
-                mergeArray[indexMergeArray] = arr1[indexArr1];
-                //flag1[indexArr1] = true;
-                indexArr1++;
-            } else {
-                mergeArray[indexMergeArray] = arr2[indexArr2];
-                //flag2[indexArr2] = true;
-                indexArr2++;
+        int [] a = {1,2,3,4,5,7,8};
+        int []b = {1,2,3,7,8};
+        int []c = new int[a.length+b.length];
+        int indexa = 0, indexb = 0, indexc = 0;
+        while(indexc<c.length){
+            if(indexa == a.length) {
+                c[indexc] = b[indexb++];
+                //indexb++;
             }
-            indexMergeArray++;
-        }
-        //if (flag2[arr2.length - 1] != true)
-        if (indexArr2<arr2.length)
-        {
-            for (int i = indexArr2; i < arr2.length; i++) {
-                mergeArray[indexMergeArray] = arr2[i];
-                indexMergeArray++;
+            else if(indexb == b.length) {
+                c[indexc] = a[indexa++];
+                //indexa++;
             }
-        }
-        if(indexArr1<arr1.length){
-            for (int i = indexArr1; i < arr1.length; i++) {
-                mergeArray[indexMergeArray] = arr1[i];
-                indexMergeArray++;
+            else if(a[indexa]>b[indexb]){
+                c[indexc] = b[indexb++];
+                //indexb++;
             }
+            else {
+                c[indexc] = a[indexa++];
+                //indexa++;
+            }
+            indexc++;
         }
-
-        for (int i : mergeArray) {
+        for (int i : c) {
             System.out.println(i);
         }
     }
