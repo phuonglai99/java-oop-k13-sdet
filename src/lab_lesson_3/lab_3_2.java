1697578ebcab59f606bb33ec15ede170985d4ed9
phuonglai99 <plai34037@gmail.com>
2022-12-16 23:01:21 +0700

    optimize code for lab_3_4

--- src/lab_lesson_3/lab_3_2.java
+++ src/lab_lesson_3/lab_3_2.java
@@ -3,11 +3,11 @@ package lab_lesson_3;
 public class lab_3_2 {
     //finding min max of array//
     public static void main(String[] args) {
-        int[] arr = {1,1,10,11};
+        int[] arr = {1,0,10,11,-1,0,-1};
         int max = arr[0], min = arr[0];
         for (int i : arr) {
-            if(i>=max) max = i;
-            if(i<=min) min = i;
+            if(i>max) max = i;
+            if(i<min) min = i;
         }
         System.out.println("minimum: "+min);
         System.out.println("maximum: "+max);
