db0c74e5067deae4846ff9b478b0c759d41a46fb
phuonglai99 <plai34037@gmail.com>
2022-12-16 22:59:37 +0700

    optimize code for lab_3_4

--- src/lab_lesson_3/lab_3_4.java
+++ src/lab_lesson_3/lab_3_4.java
@@ -3,28 +3,21 @@ package lab_lesson_3;
 public class lab_3_4 {
     //merge two ordered arrays//
     public static void main(String[] args) {
-        int [] a = {1,2,3,4,5,7,8};
-        int []b = {1,2,3,7,8};
-        int []c = new int[a.length+b.length];
+        int[] a = {1, 2, 3};
+        int[] b = {1, 2, 3};
+        int[] c = new int[a.length + b.length];
         int indexa = 0, indexb = 0, indexc = 0;
-        while(indexc<c.length){
-            if(indexa == a.length) {
-                c[indexc] = b[indexb++];
-                //indexb++;
+        while (indexc < c.length) {
+            if (indexa == a.length) {
+                c[indexc++] = b[indexb++];
+            } else if (indexb == b.length) {
+                c[indexc++] = a[indexa++];
+            } else if (a[indexa] > b[indexb]) {
+                c[indexc++] = b[indexb++];
+            } else {
+                c[indexc++] = a[indexa++];
             }
-            else if(indexb == b.length) {
-                c[indexc] = a[indexa++];
-                //indexa++;
-            }
-            else if(a[indexa]>b[indexb]){
-                c[indexc] = b[indexb++];
-                //indexb++;
-            }
-            else {
-                c[indexc] = a[indexa++];
-                //indexa++;
-            }
-            indexc++;
+            //indexc++;
         }
         for (int i : c) {
             System.out.println(i);
